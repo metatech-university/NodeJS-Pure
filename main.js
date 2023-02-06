75b3b873001456fb0bb3d552def36bdaa230946b
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-02-06 20:29:11 +0000

    Fix codebase after mooving

--- main.js
+++ main.js
@@ -3,29 +3,32 @@
 const fsp = require('node:fs').promises;
 const path = require('node:path');
 
+const staticServer = require('./lib/static.js');
+const logger = require('./lib/logger.js');
+const common = require('./lib/common.js');
+const loadConfig = require('./lib/config.js');
+const load = require('./lib/load.js');
+const ws = require('./lib/ws.js');
+
 const appPath = path.join(process.cwd(), '../NodeJS-Application');
 const apiPath = path.join(appPath, './api');
 const configPath = path.join(appPath, './config');
 const staticPath = path.join(appPath, './static');
 
-const staticServer = require('./lib/static.js');
-const logger = require('./lib/logger.js');
-const common = require('./lib/common.js');
-const config = require('./lib/config.js')(configPath);
-const load = require('./lib/load.js');
-const db = require('./lib/db.js')(config.db);
-const server = require('./lib/ws.js');
-
-const sandbox = {
-  api: Object.freeze({}),
-  db: Object.freeze(db),
-  console: Object.freeze(logger),
-  common: Object.freeze(common),
-};
-
-const routing = {};
-
 (async () => {
+  const sandbox = {
+    console: Object.freeze(logger),
+    common: Object.freeze(common),
+  };
+
+  const config = await loadConfig(configPath, sandbox);
+  const db = require('./lib/db.js')(config.db);
+
+  sandbox.api = Object.freeze({});
+  sandbox.db = Object.freeze(db);
+
+  const routing = {};
+
   const files = await fsp.readdir(apiPath);
   for (const fileName of files) {
     if (!fileName.endsWith('.js')) continue;
@@ -35,6 +38,6 @@ const routing = {};
   }
 
   const [port] = config.server.ports;
-  staticServer(staticPath, port, logger);
-  server(routing, port, logger);
+  const server = staticServer(staticPath, port, logger);
+  ws(routing, server, logger);
 })();
