da7f4100055815962b581cbe9249e6c4968c88e8
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-02-07 02:07:26 +0200

    Optimize loader: add loadDir

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const path = require('node:path');
 const staticServer = require('./lib/static.js');
 const logger = require('./lib/logger.js');
 const common = require('./lib/common.js');
-const loadConfig = require('./lib/config.js');
-const load = require('./lib/load.js');
+const { load, loadDir } = require('./lib/load.js');
 const ws = require('./lib/ws.js');
 
 const appPath = path.join(process.cwd(), '../NodeJS-Application');
@@ -21,21 +20,13 @@ const staticPath = path.join(appPath, './static');
     common: Object.freeze(common),
   };
 
-  const config = await loadConfig(configPath, sandbox);
+  const config = await loadDir(configPath, sandbox);
   const db = require('./lib/db.js')(config.db);
 
   sandbox.api = Object.freeze({});
   sandbox.db = Object.freeze(db);
 
-  const routing = {};
-
-  const files = await fsp.readdir(apiPath);
-  for (const fileName of files) {
-    if (!fileName.endsWith('.js')) continue;
-    const filePath = path.join(apiPath, fileName);
-    const serviceName = path.basename(fileName, '.js');
-    routing[serviceName] = await load(filePath, sandbox);
-  }
+  const routing = await loadDir(apiPath, sandbox);
 
   const [port] = config.server.ports;
   const server = staticServer(staticPath, port, logger);
