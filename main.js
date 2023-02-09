305306b867d5bc51719f6073e68280e43ab522cf
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-02-09 18:30:08 +0200

    Move loader and server to ./src

--- main.js
+++ main.js
@@ -3,11 +3,12 @@
 const fsp = require('node:fs').promises;
 const path = require('node:path');
 
-const staticServer = require('./lib/static.js');
 const logger = require('./lib/logger.js');
 const common = require('./lib/common.js');
-const { load, loadDir } = require('./lib/load.js');
-const ws = require('./lib/ws.js');
+
+const staticServer = require('./src/static.js');
+const { load, loadDir } = require('./src/load.js');
+const ws = require('./src/ws.js');
 
 const appPath = path.join(process.cwd(), '../NodeJS-Application');
 const apiPath = path.join(appPath, './api');
