043542f667561f79df20c097f31b19071d1e4acf
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-03-20 23:55:35 +0200

    Implement `Client` class

--- src/ws.js
+++ src/ws.js
@@ -1,29 +1,18 @@
 'use strict';
 
 const { Server } = require('ws');
+const { Client } = require('../client.js');
 
 module.exports = (routing, server, console) => {
   const ws = new Server({ server });
 
   ws.on('connection', (connection, req) => {
-    const ip = req.socket.remoteAddress;
-    connection.on('message', async (message) => {
-      const obj = JSON.parse(message);
-      const { name, method, args = [] } = obj;
-      const entity = routing[name];
-      if (!entity) return connection.send('"Not found"', { binary: false });
-      const handler = entity[method]?.method;
-      if (!handler) return connection.send('"Not found"', { binary: false });
-      const json = JSON.stringify(args);
-      const parameters = json.substring(1, json.length - 1);
-      console.log(`${ip} ${name}.${method}(${parameters})`);
-      try {
-        const result = await handler(...args);
-        connection.send(JSON.stringify(result), { binary: false });
-      } catch (err) {
-        console.error(err);
-        connection.send('"Server error"', { binary: false });
-      }
+    const client = new Client(req, null, connection);
+    connection.on('message', (data) => {
+      client.message(data);
+    });
+    connection.on('close', () => {
+      client.destroy();
     });
   });
 
