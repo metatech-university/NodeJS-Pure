c6b7811a7078aa16f271b9942a8ac42cf9179537
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-03-24 02:36:00 +0200

    Move `Client` class to `server.js`

--- src/server.js
+++ src/server.js
@@ -3,11 +3,151 @@
 const http = require('node:http');
 const fs = require('node:fs');
 const path = require('node:path');
+const crypto = require('node:crypto');
+const { EventEmitter } = require('node:events');
 const { Server } = require('ws');
-const { receiveBody } = require('../lib/common.js');
-const { Client } = require('./client.js');
+const { receiveBody, jsonParse } = require('../lib/common.js');
 const transport = require('./transport.js');
 const { HttpTransport, WsTransport, MIME_TYPES, HEADERS } = transport;
+
+class Session {
+  constructor(token, data) {
+    this.token = token;
+    this.state = { ...data };
+  }
+}
+
+const sessions = new Map(); // token: Session
+
+class Context {
+  constructor(client) {
+    this.client = client;
+    this.uuid = crypto.randomUUID();
+    this.state = {};
+    this.session = client?.session || null;
+  }
+}
+
+class Client extends EventEmitter {
+  #console;
+  #transport;
+  #routing;
+  #eventId;
+
+  constructor(console, transport, routing) {
+    super();
+    this.#console = console;
+    this.#transport = transport;
+    this.#routing = routing;
+    this.#eventId = 0;
+    this.ip = transport.ip;
+    this.session = null;
+  }
+
+  get token() {
+    if (this.session === null) return '';
+    return this.session.token;
+  }
+
+  emit(name, data) {
+    if (name === 'close') {
+      super.emit(name, data);
+      return;
+    }
+    this.send({ event: --this.#eventId, [name]: data });
+  }
+
+  initializeSession(token, data = {}) {
+    if (this.session) sessions.delete(this.session.token);
+    this.session = new Session(token, data);
+    sessions.set(token, this.session);
+    return true;
+  }
+
+  finalizeSession(token) {
+    const session = sessions.get(token);
+    if (!session) return false;
+    if (!this.session) return false;
+    sessions.delete(this.session.token);
+    this.session = null;
+    return true;
+  }
+
+  startSession(token, data = {}) {
+    this.initializeSession(token, data);
+    return true;
+  }
+
+  restoreSession(token) {
+    const session = sessions.get(token);
+    if (!session) return false;
+    this.session = session;
+    return true;
+  }
+
+  message(data) {
+    const packet = jsonParse(data);
+    if (!packet) {
+      const error = new Error('JSON parsing error');
+      this.error(500, { error, pass: true });
+      return;
+    }
+    const [callType] = Object.keys(packet);
+    if (callType === 'call') {
+      this.resumeCookieSession();
+      const [callType, target] = Object.keys(packet);
+      const callId = parseInt(packet[callType], 10);
+      const args = packet[target];
+      if (callId && args) {
+        const [interfaceName, methodName] = target.split('/');
+        void this.rpc(callId, interfaceName, methodName, args);
+        return;
+      }
+      const error = new Error('Packet structure error');
+      this.error(400, { callId, error, pass: true });
+      return;
+    }
+    const error = new Error('Packet structure error');
+    this.error(500, { error, pass: true });
+  }
+
+  async rpc(callId, interfaceName, methodName, args) {
+    const [iname, ver = '*'] = interfaceName.split('.');
+    const proc = this.routing.getMethod(iname, ver, methodName);
+    if (!proc) {
+      this.error(404, { callId });
+      return;
+    }
+    const context = new Context(this);
+    if (!this.session && proc.access !== 'public') {
+      this.error(403, { callId });
+      return;
+    }
+    let result = null;
+    try {
+      result = await proc.invoke(context, args);
+    } catch (error) {
+      if (error.message === 'Timeout reached') {
+        error.code = error.httpCode = 408;
+      }
+      this.error(error.code, { callId, error });
+      return;
+    }
+    if (result?.constructor?.name === 'Error') {
+      const { code, httpCode } = result;
+      this.error(code, { callId, error: result, httpCode: httpCode || 200 });
+      return;
+    }
+    this.send({ callback: callId, result });
+    this.#console.log(`${this.ip}\t${interfaceName}/${methodName}`);
+  }
+
+  destroy() {
+    this.emit('close');
+    if (!this.session) return;
+    sessions.delete(this.session.token);
+  }
+}
 
 const serveStatic = (staticPath) => async (req, res) => {
   const url = req.url === '/' ? '/index.html' : req.url;
