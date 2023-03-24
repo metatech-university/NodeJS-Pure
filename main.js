91d57d6a825c24e93d4db11d47a717a34341e2ef
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-03-24 03:20:23 +0200

    Rename `load.js` to `loader.js`

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('node:path');
 const logger = require('./lib/logger.js');
 const common = require('./lib/common.js');
 
-const { loadDir } = require('./src/load.js');
+const { loadDir } = require('./src/loader.js');
 const { createServer } = require('./src/server.js');
 
 const appPath = path.join(process.cwd(), '../NodeJS-Application');
