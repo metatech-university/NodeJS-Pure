deb7fb1c368865d49df224ff95754c80c93b75a4
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-07 03:37:51 +0300

    Use Map in loader

--- src/loader.js
+++ src/loader.js
@@ -20,14 +20,21 @@ const load = async (filePath, sandbox, contextualize = false) => {
 
 const loadDir = async (dir, sandbox, contextualize = false) => {
   const files = await fsp.readdir(dir, { withFileTypes: true });
-  const container = {};
+  const container = new Map();
   for (const file of files) {
     const { name } = file;
     if (file.isFile() && !name.endsWith('.js')) continue;
     const location = path.join(dir, name);
-    const key = path.basename(name, '.js');
+    const prefix = path.basename(name, '.js');
     const loader = file.isFile() ? load : loadDir;
-    container[key] = await loader(location, sandbox, contextualize);
+    const exp = await loader(location, sandbox, contextualize);
+    if (exp.constructor.name === 'Map') {
+      for (const [key, value] of exp) {
+        container.set(prefix + '.' + key, value);
+      }
+    } else {
+      container.set(prefix, exp);
+    }
   }
   return container;
 };
