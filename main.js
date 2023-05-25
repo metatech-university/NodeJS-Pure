774a5f39fb4334128577958ee8a41b68e3d9ef00
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-25 17:43:14 +0300

    Read application path from .applications file

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fsp = require('node:fs').promises;
 const path = require('node:path');
 
 const console = require('./lib/logger.js');
@@ -8,11 +9,13 @@ const common = require('./lib/common.js');
 const { loadDir, createRouting } = require('./src/loader.js');
 const { Server } = require('./src/server.js');
 
-const appPath = path.join(process.cwd(), '../NodeJS-Application');
 const api = Object.freeze({});
 const sandbox = { console, common, api, db: null };
 
 (async () => {
+  const applications = await fsp.readFile('.applications', 'utf8');
+  const appPath = path.join(process.cwd(), applications.trim());
+
   const configPath = path.join(appPath, './config');
   const config = await loadDir(configPath, sandbox);
 
